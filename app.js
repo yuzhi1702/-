b1a84d9832cb70031a091caa0ac27e0b34b88d7a
1214029722@qq.com <1214029722@qq.com>
2017-06-30 17:19:07 +0800

    'liuhong第四次修改数据'

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * Created by YZTC on 2017/6/29.
  */
-angular.module('mls',['ui.router','angularCSS','homeModule','mineModule','classifyModule','shopcarModule','subpageModule'])
+angular.module('mls',['ui.router','angularCSS','homeModule','mineModule','classifyModule','shopcarModule','subpageModule','skirtModule','shoeModule','underwearModule','hotModule','slipperModule'])
     .config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/home');
     })
---
+++ conmpent/classify/hot/hot.js
@@ -0,0 +1,26 @@
+/**
+ * Created by lh1214029722 on 2017/6/30.
+ */
+angular.module('hotModule',['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('hot', {
+                url: '/hot',
+                templateUrl: 'conmpent/classify/hot/hot.html',
+                controller:'hotCtrl',
+                css:['conmpent/classify/hot/hot.css']
+            })
+    })
+    .service('getData5',['$http',function ($http) {
+        this.get=function (data) {
+            return $http.get(data);
+        }
+    }])
+    .controller("hotCtrl",["$scope",'$http','getData5',function ($scope,$http,getData5) {
+
+        getData5.get('json/zi6.json').then(function (rea) {
+            $scope.arr = rea.data.data.list;
+
+            console.log(rea.data.data.list);
+        })
+    }])
