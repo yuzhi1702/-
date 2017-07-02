c490017e9c6e011c5e7bb0165351467d47f388f7
1214029722@qq.com <1214029722@qq.com>
2017-07-02 17:54:32 +0800

    '第二次完整'

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 /**
  * Created by YZTC on 2017/6/29.
  */
-angular.module('mls',['ui.router','angularCSS','homeModule','mineModule','classifyModule','shopcarModule','subpageModule','skirtModule','shoeModule','underwearModule','hotModule','slipperModule','tuoModule','conclusionModule'])
+angular.module('mls',['ui.router','angularCSS','me-lazyload','homeModule','mineModule','classifyModule','shopcarModule','subpageModule','skirtModule','shoeModule','underwearModule','hotModule','slipperModule','tuoModule','conclusionModule'])
     .config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/home');
     })
+
+
