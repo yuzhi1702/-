d39185edd824e242d92058dd9f605a80bd27f1a0
18062616073 <765718723@qq.com>
2017-07-03 11:52:43 +0800

    'resovereject'

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 /**
  * Created by YZTC on 2017/6/29.
  */
-<<<<<<< HEAD
-angular.module('mls',['ui.router','angularCSS','homeModule','mineModule','classifyModule','shopcarModule','subpageModule','loginModule','goodsModule','shopsModule','registerModule'])
-=======
-angular.module('mls',['ui.router','angularCSS','me-lazyload','homeModule','mineModule','classifyModule','shopcarModule','subpageModule','skirtModule','shoeModule','underwearModule','hotModule','slipperModule','registerModule','tuoModule','conclusionModule','loginModule'])
->>>>>>> 2c1a00dc10eb5a375b1aabe8d7cd0d5fccbb3138
+angular.module('mls',['ui.router','angularCSS','me-lazyload','homeModule','mineModule','classifyModule','shopcarModule','subpageModule','loginModule','goodsModule','shopsModule','registerModule','skirtModule','shoeModule','underwearModule','hotModule','slipperModule','registerModule','tuoModule','conclusionModule','loginModule'])
     .config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/home');
     })
